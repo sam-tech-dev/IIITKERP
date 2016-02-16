e98219f948c67d8218ecd0c74adaf371ed5815b3
meghagupta0401 <meghagupta.mg04@gmail.com>
2016-02-15 19:05:53 -0800

    Create a new json file methods to set add attributes value etc. in json format

---
+++ sources/main/exceptions/ConfigException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package exceptions;
+
+/**
+ * @author   Shubhi
+ *
+ */
+public class ConfigException extends Exception {
+	private static final long serialVersionUID = -640204991518160295L;
+
+	public ConfigException(String exception_message){
+		super(exception_message);
+        System.out.println(exception_message);		
+	}
+
+}
