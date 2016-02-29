737bf58178bb71f296a061c2a9339348988c5dac
anshulabisht <bisht.anshula@gmail.com>
2016-02-29 11:44:06 +0530

    Notifications

--- src/main/webapp/dist/js/notifications.js
+++ src/main/webapp/dist/js/notifications.js
@@ -1,6 +1,5 @@
-function authenticate(username,password) {
-	
-	// to support all browsers
+		
+   // to support all browsers
 	var xmlhttp;
 	try{
 		xmlhttp = new XMLHttpRequest();
@@ -18,25 +17,34 @@ function authenticate(username,password) {
 		}
 	}	
 	
-	if(xmlhttp){	
+	if(xmlhttp){
+		alert("xmlhttp object");
 	    xmlhttp.onreadystatechange=function() {
+	    	
 	        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-	        	
 				try{
 	        	data=JSON.parse(xmlhttp.responseText);
-	        	///
+	        	var	notification;
+	        	document.getElementById("notifications").innerHTML="You have "+data.length+" notifications";
+	        	document.getElementById("notif_list").innerHTML="";
+				for(var i=0;i<data.length;i++){
+					    var notif_temp=document.getElementById("notif_template");
+					    notif_temp.getElementsByClassName("notif_msg")[0].innerHTML=data[i].message;
+						document.getElementById("notif_list").innerHTML+=notif_temp.innerHTML;
+						
+					}
 			   }
 				catch(exception){
 
 				}
 			}
 	        if(xmlhttp.status == 404)
-				$("#error_password").html("Could not connect to server");
+				alert("Could not connect to server");
 			}
-	    xmlhttp.open("POST","RetriveNotifications",true);
+	    var uid=1;
+	    xmlhttp.open("POST","../RetriveNotifications",true);
 		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	    xmlhttp.send("userid=" +userid);
+	    xmlhttp.send("userid="+uid);
 	}
 	
-	return false;
-}
+	
